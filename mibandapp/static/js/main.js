ff53c8f6ee9cb8a9c248d0dfbd589508f4c515e5
Alordiah Keno <Keno@Alordiahs-MacBook-Pro.local>
2018-08-03 14:16:47 +0100

    minor script correction

--- mibandapp/static/js/main.js
+++ mibandapp/static/js/main.js
@@ -647,8 +647,10 @@
     });
 
     $('#site-wrapper').on('click', function(e){
-        e.preventDefault()
-        $('body').removeClass('menu-active')
+        if( $('body').hasClass('menu-active') ){
+            e.preventDefault()
+            $('body').removeClass('menu-active')
+        }
     });
 
 })(window, document, jQuery)
